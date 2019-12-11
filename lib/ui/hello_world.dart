85e3642d5a8e9491f1c1b701a620e71bca34dc85
BlissYang91 <2812420513@qq.com>
2019-12-11 17:28:53 +0800

    通过改变组件状态处理用户交互，点击改变状态的交互

--- lib/ui/hello_world.dart
+++ lib/ui/hello_world.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+//如果用户与 widget 交互，widget 会发生变化，那么它就是_有状态的_,此处暂时不需要交互，就是无状态的
 class MyApp extends StatelessWidget{
 
   //  1⃣标题行组件
@@ -31,11 +32,13 @@ class MyApp extends StatelessWidget{
             ],
           ),
         ),
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
+//       使用有状态的组件替代静态组件，处理用户交互问题
+       FavoriteWidget(),
+//        Icon(
+//          Icons.star,
+//          color: Colors.red[500],
+//        ),
+//        Text('41'),
       ],
     ),
   );
@@ -117,4 +120,67 @@ class MyApp extends StatelessWidget{
       ),
     );
   }
+
+
+}
+
+//如果用户与 widget 交互，widget 会发生变化，那么它就是_有状态的
+//FavoriteWidget 类管理自己的状态，因此它通过重写 createState() 来创建状态对象。框架会在构建 widget 时调用 createState()。
+//将相关状态关联的组件放在同一个类中进行统一管理
+class FavoriteWidget extends StatefulWidget{
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+
+//  @override
+//  State<StatefulWidget> createState() {
+//    // 创建State<StatefulWidget> 的子类 _FavoriteWidgetState代替
+//    return null;
+//  }
+
+}
+// 创建State<StatefulWidget> 的子类
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+              icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+              onPressed: _toggleFavorite,//点击调用方法去改变状态，调用setState方法，重新build
+              color: Colors.red[500],
+          ),
+        ),
+//     SizedBox固定宽度位置
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  /**
+   * 按下 IconButton 时会调用 _toggleFavorite() 方法，然后它会调用 setState()。
+   * 调用 setState() 是至关重要的，因为这告诉框架，widget 的状态已经改变，应该重绘.
+   * 就是重新执行build方法
+   */
+  void _toggleFavorite(){
+    setState(() {
+      if(_isFavorited){
+        _favoriteCount -= 1;
+        _isFavorited  = false;
+      }else{
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
 }
